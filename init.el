5665362b7e8646de3bf4baaef07a9dea04bb3810
Dan Aloni <alonid@gmail.com>
2014-12-18 20:16:35 +0200

    Use smooth-scroll

--- init.el
+++ init.el
@@ -399,6 +399,11 @@
 
 (require 'magit)
 
+(require 'smooth-scrolling)
+(setq smooth-scroll-margin 5)
+(setq scroll-conservatively 9999
+      scroll-preserve-screen-position t)
+
 ;; Markdown
 
 (require 'markdown-mode)
